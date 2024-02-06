b8a964a7b1510fd105338569262f9ee37ef13b0a
mlevitorresv <levitorres66@gmail.com>
2024-02-06 10:13:42 +0100

    ADD: admin.py config for view models in admin panel

--- dashboard/hotelMiranda/admin.py
+++ dashboard/hotelMiranda/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from hotelMiranda.models import * 
 
 # Register your models here.
+admin.site.register(Rooms)
+admin.site.register(Booking)
+admin.site.register(Contact)
