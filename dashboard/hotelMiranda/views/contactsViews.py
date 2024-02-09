67a5e9829e975aab524651e2c2b561692174277f
mlevitorresv <levitorres66@gmail.com>
2024-02-09 11:48:33 +0100

    RM: view_contacts

--- dashboard/hotelMiranda/views/contactsViews.py
+++ dashboard/hotelMiranda/views/contactsViews.py
@@ -6,15 +6,6 @@ import datetime
 
 
 # Create your views here.
-def view_contacts(request):
-    data = Contact.objects.all()
-    
-    results = []
-    for contact in data:
-        results.append(f'ID: {contact.id}, photo: {contact.photo}, name: {contact.name}, email: {contact.email}, phone: {contact.phone}, comment: {contact.comment}, date: {contact.date}, dateTime: {contact.dateTime}, archived: {contact.archived}')
-    
-    return HttpResponse(results)
-
 def post_contact(request):
     if request.method == 'POST':
         form = contactForm(request.POST)
